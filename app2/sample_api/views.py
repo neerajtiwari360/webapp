bdfa9a13a67852eaeaae756be8330ad1f6b193ad
PoornaTheja <ramesh.poornatheja.cse20@itbhu.ac.in>
2022-12-22 18:14:15 +0530

    removed repeated code

--- app2/sample_api/views.py
+++ app2/sample_api/views.py
@@ -55,38 +55,37 @@ def submit1(request):
         
         mask_img = cv2.imread("media/mask/test.png")
         
-        out_img = mask_img
-        out_img = out_img/255
+        # out_img = mask_img
+        # out_img = out_img/255
 
-        out_img[out_img > 0.9] = 1
-        out_img[out_img <= 0.9] = 0
+        # out_img[out_img > 0.9] = 1
+        # out_img[out_img <= 0.9] = 0
 
-        shape = out_img.shape
-        a_layer_init = np.ones(shape = (shape[0],shape[1],1))
-        mul_layer = np.expand_dims(out_img[:,:,0],axis=2)
-        a_layer = mul_layer*a_layer_init
-        rgba_out = np.append(out_img,a_layer,axis=2)
+        # shape = out_img.shape
+        # a_layer_init = np.ones(shape = (shape[0],shape[1],1))
+        # mul_layer = np.expand_dims(out_img[:,:,0],axis=2)
+        # a_layer = mul_layer*a_layer_init
+        # rgba_out = np.append(out_img,a_layer,axis=2)
 
-        inp_img = np_img
-        inp_img = inp_img/255
+        # inp_img = np_img
+        # inp_img = inp_img/255
 
-        a_layer = np.ones(shape = (shape[0],shape[1],1))
-        rgba_inp = np.append(inp_img,a_layer,axis=2)
+        # a_layer = np.ones(shape = (shape[0],shape[1],1))
+        # rgba_inp = np.append(inp_img,a_layer,axis=2)
 
-        rem_back = (rgba_inp*rgba_out)
-        rem_back_scaled = rem_back*255
+        # rem_back = (rgba_inp*rgba_out)
+        # rem_back_scaled = rem_back*255
   
-        # print("**!@#***")
         
-        rem_img1 = cv2.imencode('.png', rem_back_scaled)[1]
-        modified_image_data1 = base64.b64encode(rem_img1).decode('utf-8')
+        # rem_img1 = cv2.imencode('.png', rem_back_scaled)[1]
+        # modified_image_data1 = base64.b64encode(rem_img1).decode('utf-8')
         
         mask_img1 = cv2.imencode('.png', mask_img)[1]
         modified_image_data2 = base64.b64encode(mask_img1).decode('utf-8')
 
         # Return the modified image data as the response
         # return HttpResponse(modified_image_data, content_type='image/png')
-        return JsonResponse({"data" : modified_image_data1, "mask" : modified_image_data2})
+        return JsonResponse({"mask" : modified_image_data2})
     else:
         return HttpResponse('None')
     
